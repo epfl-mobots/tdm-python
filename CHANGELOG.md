d3f169a65eb6c77b70616dc6f764cf172548200d
Yves Piguet <yves.piguet@epfl.ch>
2021-10-13 15:57:51 +0200

    Release 0.1.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ Notable changes of tdmclient. Release versions refer to [https://pypi.org/projec
 
 ## [Unreleased]
 
+## [0.1.8] - 2021-10-13
+
 ### Added
 
 - In transpiler:
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("doc/notebooks.md", "r") as f:
 
 setup(
     name="tdmclient",
-    version="0.1.7",
+    version="0.1.8",
     author="Yves Piguet",
     packages=["tdmclient", "tdmclient.tools", "tdmclient.notebook", ],
     description="Communication with Thymio II robot via the Thymio Device Manager",
