df3e9e57427bd5341af0445c8b7a74c658d0d9d4
Yves Piguet <yves.piguet@epfl.ch>
2021-11-22 10:06:47 +0100

    In tool "watch", options to choose tdm and robot

--- tdmclient/tools/watch.py
+++ tdmclient/tools/watch.py
@@ -6,14 +6,69 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from tdmclient import ClientAsync
+import sys
+import getopt
+
+
+def help(**kwargs):
+    print("""Usage: python3 -m tdmclient.tools.watch [options]
+Watch information on robot sent by tdm
+
+Options:
+  --debug=n      display diagnostic info (0=none, 1=basic, 2=more, 3=verbose)
+  --help         display this help message and exit
+  --robotid=I    robot id; default=any
+  --robotname=N  robot name; default=any
+  --tdmaddr=H    tdm address (default: localhost or from zeroconf)
+  --tdmport=P    tdm port (default: from zeroconf)
+""", **kwargs)
+
 
 if __name__ == "__main__":
 
-    with ClientAsync(debug=2) as client:
+    debug = 2  # display all messages received from the tdm by default
+    tdm_addr = None
+    tdm_port = None
+    robot_id = None
+    robot_name = None
+
+    try:
+        arguments, values = getopt.getopt(sys.argv[1:],
+                                          "",
+                                          [
+                                              "debug=",
+                                              "help",
+                                              "robotid=",
+                                              "robotname=",
+                                              "tdmaddr=",
+                                              "tdmport=",
+                                          ])
+    except getopt.error as err:
+        print(str(err), file=sys.stderr)
+        sys.exit(1)
+    for arg, val in arguments:
+        if arg == "--help":
+            help()
+            sys.exit(0)
+        elif arg == "--debug":
+            debug = int(val)
+        elif arg == "--robotid":
+            robot_id = val
+        elif arg == "--robotname":
+            robot_name = val
+        elif arg == "--tdmaddr":
+            tdm_addr = val
+        elif arg == "--tdmport":
+            tdm_port = int(val)
+
+    if len(values) > 0:
+        help(file=sys.stderr)
+        sys.exit(1)
+
+    with ClientAsync(tdm_addr=tdm_addr, tdm_port=tdm_port, debug=debug) as client:
 
         async def prog():
-            await client.wait_for_node()
-            node = client.first_node()
+            node = await client.wait_for_node(node_id=robot_id, node_name=robot_name)
             await node.watch(flags=0x3f)  # all
             await client.sleep()
 
