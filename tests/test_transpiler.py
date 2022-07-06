758550e3fb2c66695fdeaf30c09dae72b4b1aa85
Yves Piguet <yves.piguet@epfl.ch>
2022-07-06 16:51:41 +0200

    Transpiler tests for operators with variable operands

--- tests/test_transpiler.py
+++ tests/test_transpiler.py
@@ -58,6 +58,8 @@ src_aseba:
 """)
         self.assertTrue(b)
 
+    # simple assignments
+
     def test_assign_scalar_result(self):
         self.assert_transpiled_code_result(
             "a = 123",
@@ -69,6 +71,8 @@ src_aseba:
             "a = [1, 2, 3]",
             lambda getter: getter("a") == [1, 2, 3]
         )
+
+    # operators with constant operands
 
     def test_const_plus(self):
         self.assert_transpiled_code_result(
@@ -211,6 +215,152 @@ src_aseba:
     def test_const_len(self):
         self.assert_transpiled_code_result(
             "a = len([1,2,3,1,2,3])",
+            lambda getter: getter("a") == [6]
+        )
+
+    # operators with variable operands
+
+    def test_var_plus(self):
+        self.assert_transpiled_code_result(
+            "x = 1; y = 2; a = x + y",
+            lambda getter: getter("a") == [3]
+        )
+
+    def test_var_minus(self):
+        self.assert_transpiled_code_result(
+            "x = 1; y = 2; a = x - y",
+            lambda getter: getter("a") == [-1]
+        )
+
+    def test_var_times(self):
+        self.assert_transpiled_code_result(
+            "x = 2; y = 3; a = x * y",
+            lambda getter: getter("a") == [6]
+        )
+
+    def test_var_divide(self):
+        self.assert_transpiled_code_result(
+            "x = 7; y = 2; a = x // y",
+            lambda getter: getter("a") == [3]
+        )
+
+    def test_var_modulo(self):
+        self.assert_transpiled_code_result(
+            "x = 7; y = 5; a = x % y",
+            lambda getter: getter("a") == [2]
+        )
+
+    def test_var_bitwise_and(self):
+        self.assert_transpiled_code_result(
+            "x = 0b1100; y = 0b1010; a = x & y",
+            lambda getter: getter("a") == [0b1100 & 0b1010]
+        )
+
+    def test_var_bitwise_or(self):
+        self.assert_transpiled_code_result(
+            "x = 0b1100; y = 0b1010; a = x | y",
+            lambda getter: getter("a") == [0b1100 | 0b1010]
+        )
+
+    def test_var_bitwise_xor(self):
+        self.assert_transpiled_code_result(
+            "x = 0b1100; y = 0b1010; a = x ^ y",
+            lambda getter: getter("a") == [0b1100 ^ 0b1010]
+        )
+
+    def test_var_sl(self):
+        self.assert_transpiled_code_result(
+            "x = 7; y = 2; a = x << y",
+            lambda getter: getter("a") == [7 << 2]
+        )
+
+    def test_var_sr(self):
+        self.assert_transpiled_code_result(
+            "x = 127; y = 3; a = x >> y",
+            lambda getter: getter("a") == [127 >> 3]
+        )
+
+    def test_var_eq(self):
+        self.assert_transpiled_code_result(
+            "x = 2; p = 1; q = 2; r = 3; a = [x == p, x == q, x == r]",
+            lambda getter: getter("a") == [0, 1, 0]
+        )
+
+    def test_var_ne(self):
+        self.assert_transpiled_code_result(
+            "x = 2; p = 1; q = 2; r = 3; a = [x != p, x != q, x != r]",
+            lambda getter: getter("a") == [1, 0, 1]
+        )
+
+    def test_var_gt(self):
+        self.assert_transpiled_code_result(
+            "x = 2; p = 1; q = 2; r = 3; a = [x > p, x > q, x > r]",
+            lambda getter: getter("a") == [1, 0, 0]
+        )
+
+    def test_var_ge(self):
+        self.assert_transpiled_code_result(
+            "x = 2; p = 1; q = 2; r = 3; a = [x >= p, x >= q, x >= r]",
+            lambda getter: getter("a") == [1, 1, 0]
+        )
+
+    def test_var_lt(self):
+        self.assert_transpiled_code_result(
+            "x = 2; p = 1; q = 2; r = 3; a = [x < p, x < q, x < r]",
+            lambda getter: getter("a") == [0, 0, 1]
+        )
+
+    def test_var_le(self):
+        self.assert_transpiled_code_result(
+            "x = 2; p = 1; q = 2; r = 3; a = [x <= p, x <= q, x <= r]",
+            lambda getter: getter("a") == [0, 1, 1]
+        )
+
+    def test_var_and(self):
+        self.assert_transpiled_code_result(
+            "zero = 0; one = 1; a = [zero and zero, zero and one, one and zero, one and one]",
+            lambda getter: getter("a") == [0, 0, 0, 1]
+        )
+
+    def test_var_or(self):
+        self.assert_transpiled_code_result(
+            "zero = 0; one = 1; a = [zero or zero, zero or one, one or zero, one or one]",
+            lambda getter: getter("a") == [0, 1, 1, 1]
+        )
+
+    def test_var_uplus(self):
+        self.assert_transpiled_code_result(
+            "x = 5; a = +x",
+            lambda getter: getter("a") == [5]
+        )
+
+    def test_var_uminus(self):
+        self.assert_transpiled_code_result(
+            "x = 5; a = -x",
+            lambda getter: getter("a") == [-5]
+        )
+
+    def test_var_cpl(self):
+        self.assert_transpiled_code_result(
+            "x = 123; a = ~x",
+            lambda getter: getter("a") == [-1 - 123]
+        )
+
+    def test_var_not(self):
+        self.assert_transpiled_code_result(
+            "x = False; y = True; a = [not x, not y]",
+            lambda getter: getter("a") == [1, 0]
+        )
+
+    def test_var_abs(self):
+        self.assert_transpiled_code_result(
+            "x = -123; y = 456; a = [abs(x), abs(y)]",
+            lambda getter: getter("a") == [123, 456]
+        )
+
+    def test_var_len(self):
+        self.assert_transpiled_code_result(
+            "lst = [1,2,3,1,2,3]; a = len(lst)",
             lambda getter: getter("a") == [6]
         )
 
