339e9407e431dd1db9f6768ff47fe04d49c2f2d4
Yves Piguet <27820118+ypiguet-epfl@users.noreply.github.com>
2023-09-25 23:14:44 +0200

    Release 0.1.21

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ Notable changes of tdmclient. Release versions refer to [https://pypi.org/projec
 
 ## [Unreleased]
 
+## [0.1.21] - 2023-09-25
+
 ### Fixed
 
 - In transpiler, event name fixed in code generated for `emit`
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ for filename in [
 
 setup(
     name="tdmclient",
-    version="0.1.20",
+    version="0.1.21",
     author="Yves Piguet",
     packages=["tdmclient", "tdmclient.tools", "tdmclient.notebook", ],
     description="Communication with Thymio II robot via the Thymio Device Manager",
